4af0ef09dcfae82c03fb1b4e570054346f01609d
Akash Agarwal <51333155+akashag55@users.noreply.github.com>
2022-12-27 16:50:31 +0530

    added server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("http://codex-gwi9.vercel.app/", {
+  const response = await fetch("https://codex-gwi9.vercel.app/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
